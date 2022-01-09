ada6be5abedeeab5f8d428c02ef37e06424e51cf
Qualeke Singleton <squaleke@yahoo.com>
2022-01-09 13:35:45 -0500

    add commet

--- page3.php
+++ page3.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+// You can now see that the value of $_SESSION['test-session'] is empty
 echo 'The content of $_SESSION[\'test-session\'] is ' . $_SESSION['test_session'] . "<br/>";
 
 session_destroy();
